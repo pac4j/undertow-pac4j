c2c8a9d6802187869aefa27196dc68c10cfe7b21
Jérôme LELEU <leleuj@gmail.com>
2020-09-11 11:08:46 +0200

    uncomment deploy

--- travis/deploy-to-sonatype.sh
+++ travis/deploy-to-sonatype.sh
@@ -6,6 +6,6 @@ echo -e "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 # Only invoke the deployment to Sonatype when it's not a PR and only for main branches
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" =~ ^(master|.*0\.x)$ ]]; then
   echo -e "Deploying SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
-  #mvn deploy --settings travis/settings.xml
+  mvn deploy --settings travis/settings.xml
   echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
 fi
